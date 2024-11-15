276d3fa5aa674d6e16989c0cad7ea759297a0982
thanhnhan <118108042+thanhnhanzxc@users.noreply.github.com>
2024-11-15 23:14:34 +0700

    Up date file category.service.ts
    Update function update,delete,create category

--- shopapp-angular/src/app/services/category.service.ts
+++ shopapp-angular/src/app/services/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Category } from '../models/category';
@@ -26,13 +26,25 @@ export class CategoryService {
   }
   deleteCategory(id: number): Observable<ApiResponse> {
     debugger
-    return this.http.delete<ApiResponse>(`${this.apiBaseUrl}/categories/${id}`);
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWJqZWN0IjoiMTEyMjMzNDQiLCJ1c2VySWQiOjMsInN1YiI6IjExMjIzMzQ0IiwiZXhwIjoxNzM0Mjc3MzMzfQ.QQffqT-JIIESyAJsu-7ZOf5hxx6aAZeH6e5Qfr8XlKM';
+    const headers = new HttpHeaders({
+      'Authorization' : `Bearer ${token}`,
+    })
+    return this.http.delete<ApiResponse>(`${this.apiBaseUrl}/categories/${id}`, {headers});
   }
   updateCategory(id: number, updatedCategory: UpdateCategoryDTO): Observable<ApiResponse> {
-    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/categories/${id}`, updatedCategory);
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWJqZWN0IjoiMTEyMjMzNDQiLCJ1c2VySWQiOjMsInN1YiI6IjExMjIzMzQ0IiwiZXhwIjoxNzM0Mjc3MzMzfQ.QQffqT-JIIESyAJsu-7ZOf5hxx6aAZeH6e5Qfr8XlKM';
+    const headers = new HttpHeaders({
+      'Authorization' : `Bearer ${token}`,
+    })
+    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/categories/${id}`, updatedCategory, {headers});
   }  
   insertCategory(insertCategoryDTO: InsertCategoryDTO): Observable<ApiResponse> {
     // Add a new category
-    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/categories`, insertCategoryDTO);
+    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWJqZWN0IjoiMTEyMjMzNDQiLCJ1c2VySWQiOjMsInN1YiI6IjExMjIzMzQ0IiwiZXhwIjoxNzM0Mjc3MzMzfQ.QQffqT-JIIESyAJsu-7ZOf5hxx6aAZeH6e5Qfr8XlKM';
+    const headers = new HttpHeaders({
+      'Authorization' : `Bearer ${token}`,
+    })
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/categories`, insertCategoryDTO, {headers});
   }
 }
