9fbb795930c34f9df9681e1ec5619d0e711effcc
Nhan Dang <dangthanhnhan150204@gmail.com>
2024-11-16 14:51:26 +0700

    Update user.service.ts
    Fix access to api

--- shopapp-angular/src/app/services/user.service.ts
+++ shopapp-angular/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RegisterDTO } from '../dtos/user/register.dto';
 import { LoginDTO } from '../dtos/user/login.dto';
@@ -15,115 +15,100 @@ import { ApiResponse } from '../responses/api.response';
   providedIn: 'root'
 })
 export class UserService {
-  private apiRegister = `${environment.apiBaseUrl}/users/register`;
-  private apiLogin = `${environment.apiBaseUrl}/users/login`;
-  private apiUserDetail = `${environment.apiBaseUrl}/users/details`;
-
+  private apiBaseUrl = environment.apiBaseUrl;
+  private token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWJqZWN0IjoiMTEyMjMzNDQiLCJ1c2VySWQiOjMsInN1YiI6IjExMjIzMzQ0IiwiZXhwIjoxNzM0Mjc3MzMzfQ.QQffqT-JIIESyAJsu-7ZOf5hxx6aAZeH6e5Qfr8XlKM';
   private http = inject(HttpClient);
-  private httpUtilService = inject(HttpUtilService);  
+  private httpUtilService = inject(HttpUtilService);
+  localStorage?: Storage;
 
-  localStorage?:Storage;
-  
-  private apiConfig = {
-    headers: this.httpUtilService.createHeaders(),
-  }
-
-  constructor(        
-    @Inject(DOCUMENT) private document: Document
-  ) { 
+  constructor(@Inject(DOCUMENT) private document: Document) {
     this.localStorage = document.defaultView?.localStorage;
   }
 
-  register(registerDTO: RegisterDTO):Observable<ApiResponse> {
-    return this.http.post<ApiResponse>(this.apiRegister, registerDTO, this.apiConfig);
+  private getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+      'Content-Type': 'application/json'
+    });
   }
 
-  login(loginDTO: LoginDTO): Observable<ApiResponse> {    
-    return this.http.post<ApiResponse>(this.apiLogin, loginDTO, this.apiConfig);
+  register(registerDTO: RegisterDTO): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/users/register`, registerDTO, { headers: this.getAuthHeaders() });
   }
-  /*
-  getUserDetail(token: string): Observable<ApiResponse> {
-    debugger
-    return this.http.post<ApiResponse>(this.apiUserDetail, {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      })
-    })
+
+  login(loginDTO: LoginDTO): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/users/login`, loginDTO, { headers: this.getAuthHeaders() });
   }
-  */
+
   getUserDetail(token: string): Observable<ApiResponse> {
-    return this.http.post<ApiResponse>(this.apiUserDetail, null, {
+    return this.http.post<ApiResponse>(`${this.apiBaseUrl}/users/details`, null, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`
       })
     });
   }
-  
-  updateUserDetail(token: string, updateUserDTO: UpdateUserDTO): Observable<ApiResponse>  {
-    let userResponse = this.getUserResponseFromLocalStorage();        
-    return this.http.put<ApiResponse>(`${this.apiUserDetail}/${userResponse?.id}`,updateUserDTO,{
+
+  updateUserDetail(token: string, updateUserDTO: UpdateUserDTO): Observable<ApiResponse> {
+    const userResponse = this.getUserResponseFromLocalStorage();
+    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/users/details/${userResponse?.id}`, updateUserDTO, {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`
       })
-    })
+    });
   }
+
   saveUserResponseToLocalStorage(userResponse?: UserResponse) {
     try {
-      debugger
-      if(userResponse == null || !userResponse) {
+      if (userResponse == null) {
         return;
       }
-      // Convert the userResponse object to a JSON string
-      const userResponseJSON = JSON.stringify(userResponse);  
-      // Save the JSON string to local storage with a key (e.g., "userResponse")
-      this.localStorage?.setItem('user', userResponseJSON);  
+      const userResponseJSON = JSON.stringify(userResponse);
+      this.localStorage?.setItem('user', userResponseJSON);
       console.log('User response saved to local storage.');
     } catch (error) {
       console.error('Error saving user response to local storage:', error);
     }
   }
-  getUserResponseFromLocalStorage():UserResponse | null {
+
+  getUserResponseFromLocalStorage(): UserResponse | null {
     try {
-      // Retrieve the JSON string from local storage using the key
-      const userResponseJSON = this.localStorage?.getItem('user'); 
-      if(userResponseJSON == null || userResponseJSON == undefined) {
+      const userResponseJSON = this.localStorage?.getItem('user');
+      if (userResponseJSON == null) {
         return null;
       }
-      // Parse the JSON string back to an object
-      const userResponse = JSON.parse(userResponseJSON!);  
+      const userResponse = JSON.parse(userResponseJSON);
       console.log('User response retrieved from local storage.');
       return userResponse;
     } catch (error) {
       console.error('Error retrieving user response from local storage:', error);
-      return null; // Return null or handle the error as needed
+      return null;
     }
   }
-  removeUserFromLocalStorage():void {
+
+  removeUserFromLocalStorage(): void {
     try {
-      // Remove the user data from local storage using the key
       this.localStorage?.removeItem('user');
       console.log('User data removed from local storage.');
     } catch (error) {
       console.error('Error removing user data from local storage:', error);
-      // Handle the error as needed
     }
   }
+
   getUsers(params: { page: number, limit: number, keyword: string }): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/users`;
-    return this.http.get<ApiResponse>(url, { params: params });
+    const httpParams = new HttpParams()
+      .set('page', params.page.toString())
+      .set('limit', params.limit.toString())
+      .set('keyword', params.keyword);
+    return this.http.get<ApiResponse>(`${this.apiBaseUrl}/users`, { params: httpParams, headers: this.getAuthHeaders() });
   }
 
   resetPassword(userId: number): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/users/reset-password/${userId}`;
-    return this.http.put<ApiResponse>(url, null, this.apiConfig);
+    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/users/reset-password/${userId}`, null, { headers: this.getAuthHeaders() });
   }
 
   toggleUserStatus(params: { userId: number, enable: boolean }): Observable<ApiResponse> {
-    const url = `${environment.apiBaseUrl}/users/block/${params.userId}/${params.enable ? '1' : '0'}`;
-    return this.http.put<ApiResponse>(url, null, this.apiConfig);
+    return this.http.put<ApiResponse>(`${this.apiBaseUrl}/users/block/${params.userId}/${params.enable ? '1' : '0'}`, null, { headers: this.getAuthHeaders() });
   }
-  
 }
